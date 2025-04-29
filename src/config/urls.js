86ab0b9b7295320bec3c3297caa11c55a5e6125d
Davy Josias Scheuermann <davy.josias@gmail.com>
2025-04-29 16:54:08 -0300

    Adicionado o arquivo do token

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
 const dayjs = require('dayjs'); //lib para trabalhar com datas
 
 // acessToken do arquivo token.js
-const { acessToken } = require('token.js');
+const { acessToken } = require('./token.js');
 console.log(`acessToken: ${acessToken}`);
 
 
@@ -11,16 +11,19 @@ console.log(`obligation: ${obligation}`);
 
 
 //date_start
-const date_start = dayjs().startOf('month'); 
-console.log(date_start.format('DD/MM/YYYY'));
+const date_start = dayjs().startOf('month').format('DD/MM/YYYY'); 
+console.log(date_start);
 
 
 //date_end
-const date_end = dayjs().endOf('month');
-console.log(date_end.format('DD/MM/YYYY'));
+const date_end = dayjs().endOf('month').format('DD/MM/YYYY');
+console.log(date_end);
 
 // fields
-const fields = 'cnpj,id,dominio_code,fantasy_name,';
+const fields = 'cnpj,id,dominio_code,fantasy_name';
+console.log(`fields: ${fields}`);
 
 // obligation_enpoint
 const endpoint = 'https://app.razonet.com.br/integration/v1/companies/index'
+console.log(`endpoint: ${endpoint}`);
+
