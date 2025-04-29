e0722006f134750baadcf195da2bc45ddc20e22c
Davy Josias Scheuermann <davy.josias@gmail.com>
2025-04-29 16:44:31 -0300

    definindo as constantes para o request

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,19 @@
 const dayjs = require('dayjs'); //lib para trabalhar com datas
 
+// acessToken do arquivo token.js
+const { acessToken } = require('token.js');
+console.log(`acessToken: ${acessToken}`);
+
+
+// obligation
+const obligation = 'emissao_dare';
+console.log(`obligation: ${obligation}`);
+
+
 //date_start
-const date_start = dayjs().startOf('month');
+const date_start = dayjs().startOf('month'); 
 console.log(date_start.format('DD/MM/YYYY'));
+
 
 //date_end
 const date_end = dayjs().endOf('month');
@@ -11,6 +22,5 @@ console.log(date_end.format('DD/MM/YYYY'));
 // fields
 const fields = 'cnpj,id,dominio_code,fantasy_name,';
 
-
-
-const obligation = 'monitoramento_det';
+// obligation_enpoint
+const endpoint = 'https://app.razonet.com.br/integration/v1/companies/index'
